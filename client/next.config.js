94c11c6653468078639d39d02f1e5329cfe3ca28
Vanecek0 <Vanecek0@seznam.cz>
2023-05-17 02:25:07 +0200

    change

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,19 @@ const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack');
 
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/tmdb/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ]
+      }
+    ]
+  },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
     config.plugins = config.plugins || []
